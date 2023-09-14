5ee23e758ef9c6c297dee94295f904ff84b5d092
Isaac Hussain <isaaclearns58@gmail.com>
2023-09-14 12:59:53 +0300

    add switch to header component

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,8 +1,14 @@
+import { FaRegAddressBook } from "react-icons/fa";
+import Switch from "../atoms/Switch";
+
 const Header = () => {
   return (
-    <header>
-      <h1>Address Book</h1>
-      <div>light/dark more</div>
+    <header className="bg-secondaryDark text-primary flex justify-between py-4 px-6">
+      <span className="flex items-center gap-2">
+        <FaRegAddressBook size={30} />
+        <h1>Address Book</h1>
+      </span>
+      <Switch />
     </header>
   );
 };
