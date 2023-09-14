6538abd03c2b09a4c70cc2e6b2259f1280cf8c07
Isaac Hussain <isaaclearns58@gmail.com>
2023-09-14 12:55:58 +0300

    add copyright symbol

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const Footer = () => {
-  return <footer>Made by @isaaxh</footer>;
+  return (
+    <footer className="bg-secondaryDark text-primary text-center py-3 px-6">
+      <span>&copy;</span> isaaxh. All Rights Reserved
+    </footer>
+  );
 };
 
 export default Footer;
