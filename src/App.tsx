9e055c0360e765d786a2364ae4e1c1bef8b7153b
Isaac Hussain <isaaclearns58@gmail.com>
2023-09-14 08:36:20 +0300

    add tailwindcss to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import "./App.css";
-
-function App() {
-  return (
-    <>
-      <div>Hello world</div>
-    </>
-  );
-}
+const App = () => {
+  return <div className="bg-red-900">App</div>;
+};
 
 export default App;
