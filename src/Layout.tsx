406538e97b4a10a71fe3fc5bfd120dd28d139a9f
Isaac Hussain <isaaclearns58@gmail.com>
2023-09-14 12:59:20 +0300

    add content component to the layout component

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,15 @@
+import Separator from "./components/atoms/Separator";
+import Content from "./components/organisms/Content";
 import Footer from "./components/organisms/Footer";
 import Header from "./components/organisms/Header";
 
 const Layout = () => {
   return (
-    <div className="h-screen bg-blue-900 text-white font-normal font-league">
+    <div className="h-screen bg-primary flex flex-col">
       <Header />
-      <div></div>
+      <Separator />
+      <Content />
+      <Separator />
       <Footer />
     </div>
   );
