2871248f9a8eda56a53ea72ad1c582557cd84cfb
Isaac Hussain <isaaclearns58@gmail.com>
2023-09-14 12:55:11 +0300

    add tailwind classes for theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{html,js,tsx,jsx,ts}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         league: ["League Spartan", "sans-serif"],
+      },
+      colors: {
+        primary: "#f5f7f9",
+        secondary: "#001d3d",
+        secondaryDark: "#000814",
+        secondaryLight: "#003566",
+        accent: "#ffc300",
+        accentLight: "#ffd60a",
       },
     },
   },
