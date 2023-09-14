6ec6ec4697f5f2d1050326bd9cf2c94ed8c8e093
Isaac Hussain <isaaclearns58@gmail.com>
2023-09-14 09:41:12 +0300

    add custom font family

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,7 +3,7 @@ import Header from "./components/organisms/Header";
 
 const Layout = () => {
   return (
-    <div className="h-screen bg-blue-900">
+    <div className="h-screen bg-blue-900 text-white font-normal font-league">
       <Header />
       <div></div>
       <Footer />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js,tsx,jsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        league: ["League Spartan", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
